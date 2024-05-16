4f8f5cf882c9a4d4a9f31ec587f03f5bf8146250
MR.ROBOT <ballalatharvacr7@gmail.com>
2024-05-17 01:46:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+status = True
+
+
+while status:
+    option = menu.get_items()
+    user_input = input(f" What would you like? {option}").lower()
+    if user_input == "off":
+        status =False
+    elif user_input == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_input)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
